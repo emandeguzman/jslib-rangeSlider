06c58fbc4f5f411263ae4f3eaedcfe47823546dc
eman <emandeguzman@gmail.com>
2020-08-13 17:26:06 +0800

    made into multi-handle range element

--- range_slider.js
+++ range_slider.js
@@ -7,19 +7,12 @@ function rangeSlider (el, $options){
     //#region settings/options
     const props = {
         ...{
-            min_val: 0,
-            max_val: 100,
-
+            values: [0, 100],
             val_min: 0,
             val_max: 100,
             pos_min: 0,
             pos_max: null,
-            // cur_min: 0,
-            // cur_max: 0,
-            // leftmost: 0,
-            // rightmost: null,
-            onMinValueChanged: ()=>{},
-            onMaxValueChanged: ()=>{}
+            onValueChanged: ()=>{}
         },
         ...$options
     }
@@ -38,41 +31,56 @@ function rangeSlider (el, $options){
     //#endregion
 
     //#region  build ui
-    el.innerHTML = `<div class="range_slider">
-                        <div class="outer_range"></div>
-                        <div class="inner_range"></div>
-                        <div class="handle_min"></div>
-                        <div class="handle_max"></div>
-                    </div>`
-    const root = el.querySelector(".range_slider");
-    const handle_min = root.querySelector(".handle_min");
-    const handle_max = root.querySelector(".handle_max");
-    const outer_range = root.querySelector(".outer_range");
-    const inner_range = root.querySelector(".inner_range");
+    el.innerHTML = "";
+
+    const band = document.createElement("DIV");
+    band.classList.add("band");
+
+    const root = document.createElement("DIV");
+    root.classList.add("range_slider");
+    root.appendChild(band);
+    el.appendChild(root)
+
+    //add handles
+    const handles = [];
+    for (let i = 0; i < props.values.length; i++) {
+        const handle = document.createElement("DIV");
+        handle.classList.add("handle");
+        handle.setAttribute("data-value", props.values[i]);
+        handles.push(handle);
+        root.appendChild(handle);
+    }
+
     //#endregion
 
-    //#region  set position min & max val
-    // const range_slider_bounds = root.getBoundingClientRect();
-    const handle_width = handle_min.scrollWidth;
-    // props.pos_min = 0;
-    // props.pos_max = range_slider_bounds.width - handle_width;
+    //#region set position min & max limit
+    const handle_width =  handles[0].scrollWidth;
     props.pos_min = 0;
     props.pos_max = root.scrollWidth - handle_width;
     //#endregion
 
-    //#region  normalize value and pos
-    props.val_min = Math.min(props.min_val, props.val_min);
-    props.val_max = Math.max(props.max_val, props.val_max);
+    //#region  set value min & max limit
+    props.val_min = Math.min(...props.values, props.val_min);
+    props.val_max = Math.max(...props.values, props.val_max);
     //#endregion
 
     //#region position handle
-    function positionUiElements() {
-        handle_min.style.left = valToPos(props.min_val) + "px";
-        handle_max.style.left = valToPos(props.max_val) + "px";
-        inner_range.style.left = valToPos(props.min_val) + "px";
-        inner_range.style.right = props.pos_max - valToPos(props.max_val) + "px";
+    for (let i = 0; i < props.values.length; i++) {
+        handles[i].style.left = valToPos(handles[i].getAttribute("data-value")) + "px";
     }
-    positionUiElements();
+    //#endregion
+
+    //#region adjust range band
+    function adjustBand() {
+        const positions = [];
+        for (let i = 0; i < handles.length; i++) {
+            positions.push(parseInt(handles[i].style.left.replace("px", "")));
+        }
+
+        band.style.left = Math.min(...positions) + "px";
+        band.style.right = (props.pos_max - Math.max(...positions)) + "px";
+    }
+    adjustBand();
     //#endregion
 
     //#region dragging
@@ -91,7 +99,7 @@ function rangeSlider (el, $options){
             e.preventDefault();
             // get the mouse cursor position at startup:
             pos3 = e.clientX;
-            console.debug(pos3, props.pos_min);
+            // console.debug(pos3, props.pos_min);
             // pos4 = e.clientY;
             document.onmouseup = closeDragElement;
             // call a function whenever the cursor moves:
@@ -102,7 +110,7 @@ function rangeSlider (el, $options){
             e = e || window.event;
             e.preventDefault();
             // calculate the new cursor position:
-            console.debug(e.clientX)
+            // console.debug(e.clientX)
             pos1 = pos3 - e.clientX;
             // pos2 = pos4 - e.clientY;
             pos3 = e.clientX;
@@ -111,15 +119,17 @@ function rangeSlider (el, $options){
             // elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
 
             // set leftmost, rightmost limit
-            let pos_min, pos_max;
-            if (elmnt == handle_min) {
-                pos_min = props.pos_min;
-                pos_max = valToPos(props.max_val);
-            }
-            else if (elmnt == handle_max) {
-                pos_min = valToPos(props.min_val);
+            let pos_min = props.pos_min, 
                 pos_max = props.pos_max;
-            }
+            
+            // if (elmnt == handle_min) {
+            //     pos_min = props.pos_min;
+            //     pos_max = valToPos(props.max_val);
+            // }
+            // else if (elmnt == handle_max) {
+            //     pos_min = valToPos(props.min_val);
+            //     pos_max = props.pos_max;
+            // }
 
             // make sure value don't exceed min and max pos value
             let pos;
@@ -135,23 +145,31 @@ function rangeSlider (el, $options){
 
 
             elmnt.style.left = pos + "px";
-            
+
+            //#region  throw onValueChange event
+            const values = [];
+            for (let i=0; i < handles.length; i++) {
+                values.push(posToVal(handles[i].style.left.replace("px","")));
+                adjustBand();
+            }
+            props.onValueChanged(values);
+            //#endregion
 
             // compute and set current valu
             // const p = (pos-props.pos_min)/(props.pos_max-props.pos_min);
             // const new_val = (props.max_val - props.min_val) * p + props.min_val;
-            const new_val = posToVal(pos);
+            // const new_val = posToVal(pos);
             
-            if (elmnt == handle_min) {
-                props.min_val = new_val;
-                positionUiElements();
-                props.onMinValueChanged(new_val);
-            }
-            else if (elmnt == handle_max) {
-                props.max_val = new_val;
-                positionUiElements();
-                props.onMaxValueChanged(new_val);
-            }
+            // if (elmnt == handle_min) {
+            //     props.min_val = new_val;
+            //     positionUiElements();
+            //     props.onMinValueChanged(new_val);
+            // }
+            // else if (elmnt == handle_max) {
+            //     props.max_val = new_val;
+            //     positionUiElements();
+            //     props.onMaxValueChanged(new_val);
+            // }
         }
 
         function closeDragElement() {
@@ -161,7 +179,6 @@ function rangeSlider (el, $options){
         }
     }
     
-    dragElement(handle_min);
-    dragElement(handle_max);
+    handles.forEach(handle => dragElement(handle));
     //#endregion
 }
